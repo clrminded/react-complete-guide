75ebed838bf24c66762f0d5fbfff01c9ae89f6e7
clrminded <the_rocket_scientist@icloud.com>
2022-06-24 10:29:11 -0700

    made first customer component

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,7 +1,36 @@
+import './ExpenseItem.css'
+
 // components/ExpenseItem.js
 // here we have our javascript function
-function ExpenseItem() {
-    return <h2>Expense item!</h2>
+function ExpenseItem(props) {
+    /**
+     * we really want these in App.js, so we
+     * can pass them as parameters.(props)
+     * const expenseDate = new Date(2022, 3, 28);
+     * const expenseTitle = "Car Insurance";
+     * const expenseAmount = 55.32;
+     * 
+     * KEY CONCEPT: You have to know the name of the
+     * props objects argument names (title,amount)
+     * Date params order: YYYY,MM,DD
+     */
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
+
+    return (
+        <div className="expense-item">
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </div>
+    );
 }
 
 
@@ -10,4 +39,14 @@ function ExpenseItem() {
 // we need to export it, so other files can
 // import it.
 export default ExpenseItem;
+
+/**
+ * Another key thing to remember is that every 
+ * component can only have 1 ROOT element such
+ * as a div. If there is another div, it has
+ * to be extracted to another comonent.
+ * 
+ * className is a reserved word for css, so
+ * reacts jsx uses className.
+ */
 
